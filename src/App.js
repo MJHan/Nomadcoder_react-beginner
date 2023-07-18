3e625ac37f80a17b247f92aeebf4aa7266d0fff0
MJHan <hamatoto@gmail.com>
2023-07-18 14:21:49 +0900

    useEffect introduction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, counterUp] = useState(0);
+  const onClick = () => counterUp((current) => current + 1);
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("I run only once");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"continue"}></Button>
+      <h1 className={styles.title}>Welcome back! {counter}</h1>
+      <Button text={"continue"} onClick={onClick}></Button>
     </div>
   );
 }
