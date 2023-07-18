abb97db4405fad1a1bf687c047991126d8714957
MJHan <hamatoto@gmail.com>
2023-07-18 15:41:47 +0900

    useEffect DependencyList

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, counterUp] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
   const onClick = () => counterUp((current) => current + 1);
-  console.log("I run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("I run only once");
-  };
-  useEffect(iRunOnlyOnce, []);
+
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes.");
+  }, [counter, keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here..."
+      ></input>
       <h1 className={styles.title}>Welcome back! {counter}</h1>
       <Button text={"continue"} onClick={onClick}></Button>
     </div>
